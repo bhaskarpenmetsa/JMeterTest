1d36584dfd6066ea5b63f0294f1adb0b5a6a66bb
bhaskarpenmetsa <45040979+bhaskarpenmetsa@users.noreply.github.com>
2020-01-09 15:37:25 +0530

    Add files via upload

---
+++ script.groovy
@@ -0,0 +1,14 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'master', credentialsId: '1c625b59-38b8-4cd0-8f2d-07c3e1f29b1a', url: 'https://github.com/bhaskarpenmetsa/JMeterTest.git'
+				}
+        }
+    }
+}
+node {
+   stage 'Run JMeter Test'
+   bat 'C:/Users/bpenmetsa/.jenkins/apache-jmeter-4.0/bin/jmeter.bat -n -t Decisiontech-Costing-Services.jmx -l C:/Users/bpenmetsa/.jenkins/workspace/results/testResult_123.jtl'
+}
